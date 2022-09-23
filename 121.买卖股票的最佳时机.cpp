5abbec156227829b183f990b7216e90484596565
Phillweston <2436559745@qq.com>
2022-09-24 03:18:58 +0800

    Update Code 9.23

---
+++ 121.买卖股票的最佳时机.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=121 lang=cpp
+ *
+ * [121] 买卖股票的最佳时机
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if (n == 0) return 0;
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < n; i++) {
+            maxProfit = max(maxProfit, prices[i] - minPrice);
+            minPrice = min(minPrice, prices[i]);
+        }
+        return maxProfit;
+    }
+};
+// @lc code=end
+
